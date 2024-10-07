3c667461e62c02cd3b55346c61075ae4e4333e20
TRAN HOANG GIANG <giang08t3@gmail.com>
2024-10-07 18:24:59 +0900

    add source code to git

---
+++ src/test/java/com/hg/spring/BatchProcessingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProcessingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
